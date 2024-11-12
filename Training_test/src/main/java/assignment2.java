264892de26ef16ce471c6f6682fbba30ab5f82ac
Deepika <deepika@gmail.com>
2024-11-12 17:43:28 +0000

    eclipse workspace

---
+++ Training_test/src/main/java/assignment2.java
@@ -0,0 +1,10 @@
+
+public class assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Jenkins Test, move from local to remote");
+	}
+
+}
